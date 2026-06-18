352c42917e84d6a707b47f8b8593ebb1213f9baa
Viktor Rehnberg <35767167+VRehnberg@users.noreply.github.com>
2026-06-18 08:31:40 +0200

    Apply suggestions from code review
    Co-authored-by: Sam Moors <smoors@users.noreply.github.com>

--- easybuild/easyblocks/generic/dataset.py
+++ easybuild/easyblocks/generic/dataset.py
@@ -49,7 +49,7 @@ class Dataset(Binary):
             'extract_sources': [True, "Whether or not to extract data sources", CUSTOM],
             'data_install_path': [None, "Custom installation path for datasets", CUSTOM],
             'cleanup_data_sources': [False, "Whether or not to delete the data sources after installation", CUSTOM],
-            'object_storage_ignore_dirs': [None, "Directories to be excluded from object storage", CUSTOM],
+            'object_storage_ignore_dirs': [[], "List of directories to be excluded from object storage", CUSTOM],
         })
         return extra_vars
 
@@ -79,10 +79,15 @@ class Dataset(Binary):
         change_dir(self.installdir)
         object_storage = os.path.normpath(os.path.join(os.getcwd(), os.pardir, 'object_storage'))
 
-        if self.cfg['object_storage_ignore_dirs'] and self.installdir in self.cfg['object_storage_ignore_dirs']:
+        ignore_dirs = self.cfg['object_storage_ignore_dirs']
+        if not isinstance(ignore_dirs, (list, tuple)):
+            raise EasyBuildError("Incorrect value type for object_storage_ignore_dirs, should be list or tuple: ",
+                                 ignore_dirs)
+
+        if ignore_dirs and self.installdir in ignore_dirs:
             datafiles = []
         else:
-            datafiles = create_index(os.curdir, ignore_dirs=self.cfg['object_storage_ignore_dirs'])
+            datafiles = create_index(os.curdir, ignore_dirs=ignore_dirs)
 
         for datafile in datafiles:
             cks = compute_checksum(datafile, checksum_type='sha256')
